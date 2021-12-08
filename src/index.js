cc4db799c6dbd9fcc9d02690ff8ae794f5094e4d
jasonemarkson <jasonmarkson76@gmail.com>
2021-12-08 10:47:56 -0500

    finally able to keep each image to each toy's div by adding the class attribute to the img element with the toy-avatar class

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ function fetchToys() {
   fetch("http://localhost:3000/toys")
   .then(response => response.json())
   .then(toys => {
-    let toyCollection = document.querySelector("#toy-collection")
-    // toysDiv.className = "card"
-    // document.body.appendChild(toysDiv)
-    toys.map(toy =>
-      toyCollection.innerHTML += `
-      <h2>${toy.name}<h2>
-      <img src=${toy.image} />
+    const toyCollection = document.querySelector("#toy-collection")
+
+    toys.map(toy => {
+
+      let toyDiv = document.createElement('div')
+      toyDiv.className = 'card'
+      toyDiv.innerHTML += `
+      <h2>${toy.name}</h2>
+      <img src=${toy.image} class="toy-avatar"/>
       <p>${toy.likes}</p>
       <button>like-btn</button>
       `
-      )
+      toyCollection.appendChild(toyDiv)
+    })
       console.log("toys array", toys)
   }
   )
@@ -21,7 +24,8 @@ function fetchToys() {
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
-  const toyCollection = document.querySelector("#toy-collection")
+  // const toyCollection = document.querySelector("#toy-collection")
+  fetchToys();
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -31,6 +35,5 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   })
-  fetchToys();
   ;
 })
